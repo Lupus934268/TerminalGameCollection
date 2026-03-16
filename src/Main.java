f7670798974bdc68181ab9efaf8f70908bfd0046
Lupus934268 <bgefeb08@gmail.com>
2026-03-16 17:04:24 +0100

    clean-up

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        //test-code if the shuffling the Deck works
-        Solitaire solitaireGame = new Solitaire();
 
-        solitaireGame.shuffleDeck();
-        for(int i = 0; i < 52; i++) {
-            System.out.println( solitaireGame.Pile[i] );
-        }
     }
 }
