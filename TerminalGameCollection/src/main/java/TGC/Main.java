f498a7e1522ea4f57ce433fbf27eddcf65a16b94
lupus <bgefeb08@gmail.com>
2026-06-07 15:08:11 +0200

    hopium - initial commit

---
+++ TerminalGameCollection/src/main/java/TGC/Main.java
@@ -0,0 +1,7 @@
+package TGC;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "A flake for TGC, including a devShell";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          
+          # packages installed in the devShell
+          neovim
+          jdk25
+          maven
+          jetbrains.idea-oss
+          git
+
+        ];
+        shellHook = ''
+          clear
+          echo "Welcome to the devShell!!!"
+          echo
+          tree
+          PS1='\n\[\033[1;35m\][devShell]\[\033[0m\]:\w\n>> '
+          '';
+      };
+    };
+}
