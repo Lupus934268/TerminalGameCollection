5c0d1808fce4862eac65257bec74f6f3728d2e5c
lupus <bgefeb08@gmail.com>
2026-06-28 00:25:50 +0200

    starting with drawing the backround and empty spaces for cards

--- TerminalGameCollection/src/main/java/TGC/klondike_solitaire_UI.java
+++ TerminalGameCollection/src/main/java/TGC/klondike_solitaire_UI.java
@@ -10,6 +10,22 @@ import java.io.IOException;
 
 public class klondike_solitaire_UI {
 
+    private void empty_foundation_object(TextGraphics graphic, int x, int y){
+
+        // draws an 13x9 dotted border - used for foundations
+
+        graphic.putString( x, y, "⡏⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⢹" );
+        graphic.putString( x, y + 1, "⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸" );
+        graphic.putString( x, y + 2, "⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸" );
+        graphic.putString( x, y + 3, "⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸" );
+        graphic.putString( x, y + 4, "⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸" );
+        graphic.putString( x, y + 5, "⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸" );
+        graphic.putString( x, y + 6, "⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸" );
+        graphic.putString( x, y + 7, "⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸" );
+        graphic.putString( x, y + 8, "⣇⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣸" );
+
+    }
+
     public void klondike_start() throws IOException {
 
         Terminal terminal = new DefaultTerminalFactory().createTerminal();
@@ -21,9 +37,14 @@ public class klondike_solitaire_UI {
         tg.setBackgroundColor(TextColor.ANSI.BLACK); // sets backround color
         tg.fillRectangle(new TerminalPosition(0, 0), screen.getTerminalSize(), ' '); // fills the entire screen in that color
 
-        tg.putString(5, 5, "┌────────┐");
-        tg.putString(5, 6, "│        │");
-        tg.putString(5, 7, "└────────┘");
+        empty_foundation_object(tg, 2, 1); // stockPile
+
+        // foundations
+        empty_foundation_object(tg, 55, 1);
+        empty_foundation_object(tg, 70, 1);
+        empty_foundation_object(tg, 85, 1);
+        empty_foundation_object(tg, 100, 1);
+
 
         screen.refresh(); // shows what's supposed to be on screen
         screen.readInput(); // keeps the screen alive (and reads input duh)
