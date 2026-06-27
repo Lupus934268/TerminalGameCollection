aa105ec8089264919fce757762afdbf8c2552398
lupus <bgefeb08@gmail.com>
2026-06-27 21:32:51 +0200

    clean-up

--- TerminalGameCollection/src/main/java/TGC/Main.java
+++ TerminalGameCollection/src/main/java/TGC/Main.java
@@ -1,47 +1,7 @@
 package TGC;
 
-import com.googlecode.lanterna.TerminalSize;
-import com.googlecode.lanterna.TextColor;
-import com.googlecode.lanterna.gui2.*;
-import com.googlecode.lanterna.screen.Screen;
-import com.googlecode.lanterna.screen.TerminalScreen;
-import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
-import com.googlecode.lanterna.terminal.Terminal;
-
-import java.io.IOException;
-
-
 public class Main {
-    public static void main(String[] args) throws IOException {
-
-        // Setup terminal and screen layers
-        Terminal terminal = new DefaultTerminalFactory().createTerminal();
-        Screen screen = new TerminalScreen(terminal);
-        screen.startScreen();
-
-        // Create panel to hold components
-        Panel panel = new Panel();
-        panel.setLayoutManager(new GridLayout(2));
-
-        panel.addComponent(new Label("Baum"));
-        panel.addComponent(new TextBox());
-
-        panel.addComponent(new Label("lupus"));
-        panel.addComponent(new TextBox());
-
-        panel.addComponent(new Label("Busch"));
-        panel.addComponent(new TextBox());
-
-        panel.addComponent(new EmptySpace(new TerminalSize(0,0))); // Empty space underneath labels
-        panel.addComponent(new Button("KYS"));
-
-        // Create window to hold the panel
-        BasicWindow window = new BasicWindow();
-        window.setComponent(panel);
-
-        // Create gui and start gui
-        MultiWindowTextGUI gui = new MultiWindowTextGUI(screen, new DefaultWindowManager(), new EmptySpace(TextColor.ANSI.BLUE));
-        gui.addWindowAndWait(window);
+    public static void main(String[] args) {
 
     }
 }
