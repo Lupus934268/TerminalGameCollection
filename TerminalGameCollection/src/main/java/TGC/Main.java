e77ff78a5f9dbe95dc2afe0216b0e90144c02f8b
lupus <bgefeb08@gmail.com>
2026-07-08 11:07:21 +0200

    works, laterna screen resizes dynamically

--- TerminalGameCollection/src/main/java/TGC/Main.java
+++ TerminalGameCollection/src/main/java/TGC/Main.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         klondike_solitaire_UI ksUI = new klondike_solitaire_UI();
-        ksUI.klondike_start();
+
+        ksUI.klondike_start(args);
     }
 }
--- TerminalGameCollection/src/main/java/TGC/klondike_solitaire_UI.java
+++ TerminalGameCollection/src/main/java/TGC/klondike_solitaire_UI.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 
 public class klondike_solitaire_UI {
 
+    // draws a 13x9 dotted border - used for foundations
+    // hardcoded - will likely remove
     private void empty_foundation_object(TextGraphics graphic, int x, int y){
-
-        // draws an 13x9 dotted border - used for foundations
 
         graphic.putString( x, y, "⡏⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⢹" );
         graphic.putString( x, y + 1, "⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸" );
@@ -26,28 +26,74 @@ public class klondike_solitaire_UI {
 
     }
 
-    public void klondike_start() throws IOException {
+    // poker card ratio 1:1.4
+    private void card_placeholder_frame(TextGraphics graphic, int x, int y, int scale){
 
-        Terminal terminal = new DefaultTerminalFactory().createTerminal();
+        // enforcing a minimum scale of 2
+        scale = Math.max(scale, 2); // Math.max compares to ints and returns the bigger of the two
 
+        // characters are 1:2, therefore we need double the width
+        int width  = scale * 2;
+        int height = (int) Math.round(scale * 1.4);
+
+        String top = "⡏" + "⠉".repeat(width - 2) + "⢹";
+        String middle = "⡇" + "⠀".repeat(width - 2) + "⢸";
+        String bottom = "⣇" + "⣀".repeat(width - 2) + "⣸";
+
+        graphic.putString(x, y, top);
+        for (int row = 1; row < height - 1; row++) {
+            graphic.putString(x, y + row, middle);
+        }
+        graphic.putString(x, y + height - 1, bottom);
+
+    }
+
+    public void klondike_start(String[] input) throws IOException {
+
+        Terminal terminal = new DefaultTerminalFactory().createTerminal(); // creates a terminal screen
         Screen screen = new TerminalScreen(terminal); // makes a new screen object
+
+        // for running the
+        boolean running = true;
+        boolean needsRedraw = true;
+
+
         screen.startScreen(); // starts said screen object
-
         TextGraphics tg = screen.newTextGraphics(); // creates a backround for the terminal screen
-        tg.setBackgroundColor(TextColor.ANSI.BLACK); // sets backround color
-        tg.fillRectangle(new TerminalPosition(0, 0), screen.getTerminalSize(), ' '); // fills the entire screen in that color
 
-        empty_foundation_object(tg, 2, 1); // stockPile
+        while(running) {
 
-        // foundations
-        empty_foundation_object(tg, 55, 1);
-        empty_foundation_object(tg, 70, 1);
-        empty_foundation_object(tg, 85, 1);
-        empty_foundation_object(tg, 100, 1);
+            TerminalSize newSize = screen.doResizeIfNecessary();
+            if (newSize != null) {
+                needsRedraw = true;
+            }
+
+            if(needsRedraw) {
+                // draws screen
+                tg.setBackgroundColor(TextColor.ANSI.BLACK); // sets backround color
+                tg.fillRectangle(new TerminalPosition(0, 0), screen.getTerminalSize(), ' '); // fills the entire screen in that color
 
 
-        screen.refresh(); // shows what's supposed to be on screen
-        screen.readInput(); // keeps the screen alive (and reads input duh)
+                int cardScale = screen.getTerminalSize().getColumns() / (7 * 2);
+
+                card_placeholder_frame(tg, 3, 1, cardScale); // stock pile
+
+                int cardWidth = cardScale * 2;
+                int rightEdge = screen.getTerminalSize().getColumns() - 3;
+                // foundations
+                for(int i = 1; i < 5; i++) {
+                    card_placeholder_frame(tg, rightEdge - (i * cardWidth) - (2*i-2), 1, cardScale);
+                }
+
+                // tableau
+
+                screen.refresh(); // shows what's supposed to be on screen
+                needsRedraw = false;
+            }
+
+            if (screen.pollInput() != null) { running = false; }
+
+        }
 
     }
 
